3af54ade7dde7e5eb5f7632cb0499bce67613607
eseodiase <reacheseodiase@gmail.com>
2025-08-18 08:50:33 +0100

    Chat.jsx formatted

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,32 +1,38 @@
-const Chat =()=>{
-      return(
-          <section>
-                   <div className="left-chat">
-                              <p>That sounds great. I'd be happy with that.</p>
-                                       </div>
-                                                
-                                                         <div className="left-chat">
-                                                                   <p>Could you send over some pictures of your dog, please?</p>
-                                                                           </div>
-                                                                                   
-                                                                                           <div className="images">
-                                                                                                   <img src="./images/dog-image-1.jpg" alt="dog1"/>
-                                                                                                           <img src="./images/dog-image-2.jpg" alt="dog2"/>
-                                                                                                                   <img src="./images/dog-image-3.jpg" alt="dog3"/>
-                                                                                                                           </div>
-                                                                                                                                   
-                                                                                                                                           <div className="right-chat">
-                                                                                                                                                   <p>Here are a few pictures, she's a happy girl.</p>
-                                                                                                                                                           </div>
+const Chat = () => {
+        return (
+                <section>
+      <div className="left-chat">
+        <p>That sounds great. I'd be happy with that.</p>
+      </div>
 
-                                                                                                                                                                   <div className="right-chat" id="chat-2">
-                                                                                                                                                                           <p>Can you make it?</p>
-                                                                                                                                                                                   </div>
-                                                                                                                                                                                           
-                                                                                                                                                                                                   <div className="left-chat">
-                                                                                                                                                                                                           <p>She looks so happy! The time we discussed works. How long shall I take her out for?</p>
-                                                                                                                                                                                                                   </div>
-                                                                                                                                                                                                                       </section>
-                                                                                                                                                                                                                         );
-                                                                                                                                                                                                                         }
-                                                                                                                                                                                                                         export default Chat;
+      <div className="left-chat">
+        <p>Could you send over some pictures of your dog, please?</p>
+      </div>
+
+      <div className="images">
+        <img src="./images/dog-image-1.jpg" alt="dog1" />
+
+        <img src="./images/dog-image-2.jpg" alt="dog2" />
+
+        <img src="./images/dog-image-3.jpg" alt="dog3" />
+      </div>
+
+      <div className="right-chat">
+        <p>Here are a few pictures, she's a happy girl.</p>
+      </div>
+
+      <div className="right-chat" id="chat-2">
+        <p>Can you make it?</p>
+      </div>
+
+      <div className="left-chat">
+        <p>
+          She looks so happy! The time we discussed works. How long shall I take
+          her out for?
+        </p>
+      </div>
+    </section>
+        );
+};
+
+export default Chat;
