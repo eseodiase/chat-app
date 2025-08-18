9088f4da2f78c61a1d100affba54d54046e930fd
eseodiase <reacheseodiase@gmail.com>
2025-08-18 09:11:28 +0100

    ScreenTop.jsx formatted

--- src/components/ScreenTop.jsx
+++ src/components/ScreenTop.jsx
@@ -1,20 +1,25 @@
-const ScreenTop =()=>{
-      return(
-               <section className="top-screen">
-                          <div className="white-path"></div>
-                                     
-                                                <div className="ang">
-                                                             <b>&lsaquo;</b></div>
-                                                                        <div className="cir"></div>
-                                                                                   
-                                                                                              <div className="txt">
-                                                                                                           <p className="p1">Samuel Green</p>
-                                                                                                                        <p className="p2">Available to walk</p>
-                                                                                                                                   </div>
-                                                                                                                                              
-                                                                                                                                                         <div className="dots">
-                                                                                                                                                                      <b>{"\u22EE"}</b></div>
-                                                                                                                                                                               </section>
-                                                                                                                                                                                 );
-                                                                                                                                                                                 }
-                                                                                                                                                                                 export default ScreenTop;
+const ScreenTop = () => {
+  return (
+    <section className="top-screen">
+      <div className="white-path"></div>
+
+      <div className="ang">
+        <b>&lsaquo;</b>
+      </div>
+
+      <div className="cir"></div>
+
+      <div className="txt">
+        <p className="p1">Samuel Green</p>
+
+        <p className="p2">Available to walk</p>
+      </div>
+
+      <div className="dots">
+        <b>{"\u22EE"}</b>
+      </div>
+    </section>
+  );
+};
+
+export default ScreenTop;
