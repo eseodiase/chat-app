9132dc51d7447ea62f5a76502f947e2801b3c956
eseodiase <reacheseodiase@gmail.com>
2025-08-18 09:28:28 +0100

    App.jsx formatted

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,43 @@
-import './App.scss';
-import Container from './components/Container.jsx';
-import Interface from './components/Interface.jsx';
-import ScreenTop from './components/ScreenTop.jsx';
-import Chat from './components/Chat.jsx';
-import Form from './components/Form.jsx';
-import Article from './components/Article.jsx';
+import "./App.scss";
+
+import Container from "./components/Container.jsx";
+
+import Interface from "./components/Interface.jsx";
+
+import ScreenTop from "./components/ScreenTop.jsx";
+
+import Chat from "./components/Chat.jsx";
+
+import Form from "./components/Form.jsx";
+
+import Article from "./components/Article.jsx";
 
 function App() {
-  return(
-       <main>
-            <section>
-                 <div class="corner-left"></div>
-                      <div class="corner-right"></div>
-                           
-                                <Container>
-                                       <Interface>
-                                                <ScreenTop/>
-                                                         <Chat/>
-                                                                  <Form/>
-                                                                         </Interface>
-                                                                                
-                                                                                       <article>
-                                                                                                <Article/>
-                                                                                                       </article>
-                                                                                                            </Container>
-                                                                                                                 
-                                                                                                                      <div class="corner-right-for-mobile-view"></div>
-                                                                                                                           </section>
-                                                                                                                              
-                                                                                                                                   <div class="attribution">
-                                                                                                                                     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-                                                                                                                                       Coded by <a href="#">Ese Odiase</a>
-                                                                                                                                            </div>
-                                                                                                                                             </main>
-                                                                                                                                               )
-                                                                                                                                               }
+  return (
+    <main>
+      <section>
+        <div class="corner-left"></div>
 
-                                                                                                                                               export default App;
+        <div class="corner-right"></div>
+
+        <Container>
+          <Interface>
+            <ScreenTop />
+
+            <Chat />
+
+            <Form />
+          </Interface>
+
+          <article>
+            <Article />
+          </article>
+        </Container>
+
+        <div class="corner-right-for-mobile-view"></div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
